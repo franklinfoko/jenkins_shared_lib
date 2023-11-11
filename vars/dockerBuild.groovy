ab7317bc90b7a8e737c8caf597c9ec29d9699678
Franklin Foko <fokofranklin47@gmail.com>
2023-11-11 02:56:35 +0100

    ok

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,7 +2,7 @@ def call(String imageRepoName, String awsAccountId, String imageTag, String awsD
     
     sh """
      docker build -t ${imageRepoName}:${imageTag} .
-     docker tag ${imageRepoName}:${imageTag} ${ubUser}/${imageRepoName}:${ImageTag}
+     docker tag ${imageRepoName}:${imageTag} ${hubUser}/${imageRepoName}:${ImageTag}
      docker tag ${imageRepoName}:${imageTag} ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com/${imageRepoName}:${imageTag}
     """
 }
