76717b23888302f8fa40e82bfd81630dfa5643e1
Franklin Foko <fokofranklin47@gmail.com>
2023-11-11 03:25:32 +0100

    ok

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,4 +1,4 @@
-def call(String imageRepoName, String imageTag, String hubUser) {
+def call(String imageRepoName, String imageTag, String hubUser, String awsDefaultregion, String awsAccountId) {
     withCredentials([usernamePassword(
         credentialsId: 'dockerhub',
         passwordVariable: 'DOCKERHUB_PASSWORD',
@@ -7,4 +7,9 @@ def call(String imageRepoName, String imageTag, String hubUser) {
         sh "docker login -u '$DOCKERHUB_USER' -p '$DOCKERHUB_PASSWORD'"
     }
     sh "docker push ${hubUser}/${imageRepoName}:${ImageTag}"
+
+    sh """
+     aws ecr get-login-password --region ${awsDefaultregion} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com
+     docker push ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com/${imageRepoName}:${imageTag}
+    """
 }
