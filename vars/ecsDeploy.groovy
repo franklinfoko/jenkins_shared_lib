78fc25265802fd12a7aed8bec5f743c35a00fcad
Franklin Foko <fokofranklin47@gmail.com>
2023-11-11 06:52:05 +0100

    ok

---
+++ vars/ecsDeploy.groovy
@@ -0,0 +1,11 @@
+def call(String ecsService, String taskDefinition, String revision, String ecsCluster, String count) {
+
+    withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    credentialsId: 'aws-credentials',
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                ]]) {
+                    sh "aws ecs update-service --force-new-deployment --service ${ecsService} --task-definition ${taskDefinition}:${revision} --cluster ${ecsCluster}  --desired-count ${count}"
+                }
+}
