102a53683ffc556d43d9782dbe14e527ee5ff945
Franklin Foko <fokofranklin47@gmail.com>
2023-11-11 04:35:08 +0100

    ok

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,15 +1,26 @@
 def call(String imageRepoName, String imageTag, String hubUser, String awsDefaultregion, String awsAccountId) {
+    // push on docker hub
     withCredentials([usernamePassword(
         credentialsId: 'dockerhub',
         passwordVariable: 'DOCKERHUB_PASSWORD',
         usernameVariable: 'DOCKERHUB_USER'
     )]) {
-        sh "docker login -u '$DOCKERHUB_USER' -p '$DOCKERHUB_PASSWORD'"
+        sh """
+         docker login -u '$DOCKERHUB_USER' -p '$DOCKERHUB_PASSWORD'
+         docker push ${hubUser}/${imageRepoName}:${ImageTag}
+        """ 
     }
-    sh "docker push ${hubUser}/${imageRepoName}:${ImageTag}"
 
-    withCredentials([<object of type com.cloudbees.jenkins.plugins.awscredentials.AmazonWebServicesCredentialsBinding>]) {
-        sh "aws ecr get-login-password --region ${awsDefaultregion} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com"
-}
-    sh "docker push ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com/${imageRepoName}:${imageTag}"
+    // push on ECR
+    withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    credentialsId: 'aws-credentials',
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                ]]) {
+                    sh """
+                     aws ecr get-login-password --region ${awsDefaultregion} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com
+                     docker push ${awsAccountId}.dkr.ecr.${awsDefaultregion}.amazonaws.com/${imageRepoName}:${imageTag}
+                    """
+                }
 }
